ca4bc842f37091579409d06639d8038db211b414
Andrew Yuan <andrewjyuan@gmail.com>
2024-11-09 15:20:59 -0500

    Set up basic appwrite functionality
    Still need to create login/signup pages and other essential pages.

---
+++ lib/appwrite.ts
@@ -0,0 +1,32 @@
+import { Account, Client, ID } from "react-native-appwrite";
+
+export const config = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.andrew.market",
+  projectId: "672a76f90006efc0fbee",
+  databaseId: "672fb048001458368661",
+  userCollectionId: "672fb06d0021d4003463",
+  listingCollectionId: "672fbcc8001cfd981286",
+};
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+const account = new Account(client);
+
+export const createUser = () => {
+  // Register User
+  account.create(ID.unique(), "me@example.com", "password", "Jane Doe").then(
+    function (response) {
+      console.log(response);
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+};
