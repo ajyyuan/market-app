8e62a92274982917f112a178c6d4f0241bb94b7c
Andrew Yuan <andrewjyuan@gmail.com>
2024-11-10 21:59:34 -0500

    Lists listings from Appwrite; created useAppwrite
    Shows listings in FlatList on Listings and Profile pages.
    Created useAppwrite custom hook.

--- app/(tabs)/listings.tsx
+++ app/(tabs)/listings.tsx
@@ -1,13 +1,59 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, FlatList, RefreshControl } from "react-native";
+import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { getAllPosts } from "@/lib/appwrite";
+import useAppwrite from "@/lib/useAppwrite";
+import { ThemedText } from "@/components/ThemedText";
+
+interface Post {
+  $id: string;
+  eatery: string;
+  order: string;
+  addOnPrice: number;
+  bid: number;
+  quantity: number;
+  createdAt: string;
+  paymentMethod: string;
+  mode: string;
+}
 
 const Listings = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
-    <SafeAreaView>
-      <View className="my-4 mx-4">
-        <Text className="text-3xl font-bold">Listings</Text>
-      </View>
+    <SafeAreaView className="h-full">
+      <FlatList<Post>
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <View className="mt-2 mx-4">
+            <ThemedText>
+              {`${item.eatery}: ${item.order}, ${item.addOnPrice}, ${item.bid}, ${item.quantity}, ${item.createdAt}, ${item.paymentMethod}, ${item.mode}`}
+            </ThemedText>
+          </View>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-4 mx-4">
+            <ThemedText type="title">Listings</ThemedText>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <View className="my-4 mx-4">
+            <ThemedText>No Listings</ThemedText>
+          </View>
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </SafeAreaView>
   );
 };
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Account, Client, ID } from "react-native-appwrite";
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
 
 export const config = {
   endpoint: "https://cloud.appwrite.io/v1",
@@ -18,15 +18,66 @@ client
   .setPlatform(config.platform);
 
 const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
 
-export const createUser = () => {
-  // Register User
-  account.create(ID.unique(), "me@example.com", "password", "Jane Doe").then(
-    function (response) {
-      console.log(response);
-    },
-    function (error) {
-      console.log(error);
-    },
-  );
+export const createUser = async (
+  email: string,
+  password: string,
+  username: string
+) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw new Error();
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw new Error(`${error}`);
+  }
+};
+
+export const signIn = async (email: string, password: string) => {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+
+    return session;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
+};
+
+export const getAllPosts = async () => {
+  try {
+    const posts = await databases.listDocuments(
+      config.databaseId,
+      config.listingCollectionId
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(`${error}`);
+  }
 };
